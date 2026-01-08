4a685c86e2587d571954477d78c0418785fb346f
vansh-verma-dev <vanshverma6065@gmail.com>
2026-01-08 10:47:32 +0530

    Updated calculator UI and JS logic

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 const input = document.getElementById("input");
 const button = document.querySelectorAll("button");
 
-button.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.innerHTML === "AC") {
-      input.value = "";
-    } else if (button.innerHTML === "=") {
-      input.value = eval(input.value);
+button.forEach((btn) => {
+  btn.addEventListener("click", () => {
+  const value = btn.dataset.value || btn.innerText;
+
+
+    if (value === "AC") {
+      input.value = "0";
+      return;
+    } 
+    if(value === "DEL"){
+      input.value = input.value.slice(0, -1) || "0";
+      return;
+    }
+    if (value === "=") {
+      try {
+        input.value = eval(input.value);
+        return;
+      } catch {
+        input.value = "ERROR";
+        return;
+      }
+    }
+
+    if (input.value === "0") {
+      input.value = value;
     } else {
-      input.value += button.innerHTML;
+      input.value += value;
     }
   });
 });
