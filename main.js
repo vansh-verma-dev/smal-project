ec50ecf92c96faef03e2903895462dd9ff6c1d6b
vansh-verma-dev <vanshverma6065@gmail.com>
2026-01-08 13:29:00 +0530

    create new feature currency convert page

--- main.js
+++ main.js
@@ -3,17 +3,19 @@ const button = document.querySelectorAll("button");
 
 button.forEach((btn) => {
   btn.addEventListener("click", () => {
-  const value = btn.dataset.value || btn.innerText;
+    const value = btn.dataset.value || btn.innerText;
 
-
+    //     button AC all value clear
     if (value === "AC") {
       input.value = "0";
       return;
-    } 
-    if(value === "DEL"){
+    }
+    // Del btn last value num delete
+    if (value === "DEL") {
       input.value = input.value.slice(0, -1) || "0";
       return;
     }
+    // value calculation===
     if (value === "=") {
       try {
         input.value = eval(input.value);
@@ -23,7 +25,7 @@ button.forEach((btn) => {
         return;
       }
     }
-
+    // input value show
     if (input.value === "0") {
       input.value = value;
     } else {
@@ -31,3 +33,84 @@ button.forEach((btn) => {
     }
   });
 });
+// Calculator theme change function
+// Calculator theme change function
+const main = document.querySelector(".main");
+const mode = document.querySelector(".Mode");
+const btns = document.querySelectorAll('.btn');
+let darkMode = false;
+
+mode.addEventListener("click", () => {
+  if (!darkMode) {
+    main.style.backgroundColor = "black";
+    main.style.color = "#ffffff";
+    input.style.color = "#ffffff";
+    // Buttons ke liye loop
+    btns.forEach(btn => {
+      btn.style.backgroundColor = "#ffffffff";  // dark buttons
+      btn.style.color = "#000000ff";
+    });
+
+    darkMode = true;
+  } else {
+    main.style.backgroundColor = "";
+    main.style.color = "";
+    input.style.color = "#000000ff";
+    btns.forEach(btn => {
+      btn.style.backgroundColor = ""; // default
+      btn.style.color = "";           // default
+    });
+
+    darkMode = false;
+  }
+});
+const currancy = document.querySelector('.currancy');
+currancy.addEventListener('click',()=>{
+  main.innerHTML =`
+  <nav>
+        <i class="fa-solid fa-arrow-left"></i>
+        <p>Online Currency Converter</p>
+    </nav>
+     <div class="screen">
+        <form>
+            <select  id="from">
+                <option value="INR">INR</option>
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+            </select>
+            <input type="number" id="input-from" class="input" placeholder="Enter Your Amount">
+        </form>
+                <form>
+            <select  id="from">
+                <option value="INR">INR</option>
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+            </select>
+            <p id="result">00000</p>
+        </form>
+    </div>
+        <div class="buttns">
+      <button class="butns">7</button>
+       <button class="butns">8</button>
+        <button class="butns">9</button>
+    </div>
+       <div class="buttns">
+      <button class="butns">4</button>
+       <button class="butns">5</button>
+        <button class="butns">6</button>
+    </div>
+       <div class="buttns">
+      <button class="butns">1</button>
+       <button class="butns">2</button>
+        <button class="butns">3</button>
+    </div>
+       <div class="buttns">
+       <button class="butns">0</button>
+        <button class="butns">.</button>
+               <button class="butns DEL"><i class="fa-solid fa-delete-left"></i></button>
+    </div>
+  `
+  const back = document.querySelector('.fa-arrow-left').addEventListener('click',()=>{
+    window.location="../Calculator/index.html"
+  })
+})
