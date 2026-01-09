49e601db00615362b509d38d933b559200997d7e
vansh-verma-dev <vanshverma6065@gmail.com>
2026-01-09 19:29:40 +0530

    add new currancy page use html

--- main.js
+++ main.js
@@ -64,53 +64,7 @@ mode.addEventListener("click", () => {
     darkMode = false;
   }
 });
-const currancy = document.querySelector('.currancy');
-currancy.addEventListener('click',()=>{
-  main.innerHTML =`
-  <nav>
-        <i class="fa-solid fa-arrow-left"></i>
-        <p>Online Currency Converter</p>
-    </nav>
-     <div class="screen">
-        <form>
-            <select  id="from">
-                <option value="INR">INR</option>
-                <option value="USD">USD</option>
-                <option value="EUR">EUR</option>
-            </select>
-            <input type="number" id="input-from" class="input" placeholder="Enter Your Amount">
-        </form>
-                <form>
-            <select  id="from">
-                <option value="INR">INR</option>
-                <option value="USD">USD</option>
-                <option value="EUR">EUR</option>
-            </select>
-            <p id="result">00000</p>
-        </form>
-    </div>
-        <div class="buttns">
-      <button class="butns">7</button>
-       <button class="butns">8</button>
-        <button class="butns">9</button>
-    </div>
-       <div class="buttns">
-      <button class="butns">4</button>
-       <button class="butns">5</button>
-        <button class="butns">6</button>
-    </div>
-       <div class="buttns">
-      <button class="butns">1</button>
-       <button class="butns">2</button>
-        <button class="butns">3</button>
-    </div>
-       <div class="buttns">
-       <button class="butns">0</button>
-        <button class="butns">.</button>
-               <button class="butns DEL"><i class="fa-solid fa-delete-left"></i></button>
-    </div>
-  `
-  const back = document.querySelector('.fa-arrow-left').addEventListener('click',()=>{
-    window.location="../Calculator/index.html"
-  })
+ 
+const currancy = document.querySelector('.currancy').addEventListener('click',()=>{
+  window.location = "../Calculator/currency.html"
 })
